5de257b64102c4afa90c373e769a4fecb5eb4c15
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2025-11-26 19:39:25 +0100

    Add files via upload

---
+++ Ej2P2.cu
@@ -0,0 +1,111 @@
+/*
+
+Dada la siguiente operación vectorial:
+
+c[i] = (a[i] + c[i]) * (a[i] - c[i])
+
+
+¿Es esta operación paralelizable en CUDA?
+
+	- Si NO: Explica la razón técnica.
+
+	- Si SÍ: Escribe el código completo en CUDA para ejecutarla.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+
+#define N 1000000 // Tamaño del vector
+
+
+__global__ void vectorMathKernel(float *d_a, float *d_c, int n) {
+
+     int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < n) {     
+	  d_c[i] = (d_a[i] + d_c[i]) * (d_a[i] - d_c[i]);
+    }
+}
+
+
+
+
+int main(){
+
+    //CPU
+  
+    float *h_a, *h_c;
+    float *d_a, *d_c;
+
+    size_t size = N * sizeof(float);
+
+    h_a = (float *)malloc(size);
+    h_c = (float *)malloc(size);
+
+   
+  
+  //Inicializamos los vectores con datos. 
+  
+    for (int i = 0; i < N; i++) {
+        h_a[i] = 5.0f; 
+        h_c[i] = 3.0f; 
+    }
+
+
+
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_c, size);
+
+    
+    
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_c, h_c, size, cudaMemcpyHostToDevice);
+
+//-----------------------------------------------------------------------------------------------------
+    //GPU
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    printf("Lanzando kernel con %d bloques de %d hilos...%n", blocksPerGrid, threadsPerBlock);
+
+
+    vectorMathKernel<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_c, N);
+
+    
+    cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
+
+//-----------------------------------------------------------------------------------------------
+    //CPU
+
+ 
+    bool correcto = true;
+    
+    for (int i = 0; i < 5; i++) {
+                
+        printf("c[%d]: Resultado actual = %f%n", i, h_c[i]);
+        
+        if(h_c[i] != 16.0f){ 
+
+            correcto = false;
+
+        }
+    }
+  
+    if(correcto){
+        printf("%n¡Éxito! El cálculo (5+3)*(5-3)=16 es correcto.%n");
+    }else{ 
+        printf("%nError en el cálculo.%n");
+    }
+
+
+    cudaFree(d_a); 
+    cudaFree(d_c);
+    
+    free(h_a); 
+    free(h_c);
+
+    return 0;
+}
